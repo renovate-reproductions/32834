50f1b17cd795f4e3b764a2718637ef3a1ee17c4d
Bernardo Martínez <bernardo.martinez@cabify.com>
2024-12-08 06:54:14 +0100

    setup

---
+++ mix.exs
@@ -0,0 +1,22 @@
+defmodule MyProject.MixProject do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :my_project,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      deps: deps(),
+    ]
+  end
+
+  def application() do
+    []
+  end
+
+  defp deps() do
+    [
+      {:gun, "== 2.0.0", hex: :grpc_gun},
+    ]
+  end
+end
